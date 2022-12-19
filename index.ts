cec464abdbabf418f9e2755a769e7b4c764ae775
TurboTechLab <42316316+TurboTechLab@users.noreply.github.com>
2022-12-19 14:34:48 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -1,22 +1,14 @@
-import { timer, sampleTime , scan , switchMap, 
-         tap, EMPTY, merge, startWith } from 'rxjs';
-import {start, stop} from './demo-control';
+import { interval, fromEvent , auditTime , debounceTime, tap } from 'rxjs';
 
-const mockUnitChange = () => Math.round( Math.random()*10 - 4);
-//Emits random values around 30000, every 5 sec
-const mockSource = timer(1000,5000).pipe(
-                    scan( (acc, v) => acc + mockUnitChange(), 30000 ),
-                    tap(v => console.log("source value :" + v) ),
-                   );
+const searchInputElement = document.getElementById('search-input');
+//Emit event on every 'keyup'
+const searchKeyUpObservable = fromEvent(searchInputElement, 'keyup')
 
-//Filter the source and emits the latest values, every 15 sec
-const sampleToSave = mockSource.pipe(                        
-                        sampleTime(15000),
-                        tap(v => console.log("sample for saving :" + v) ),
-                     );
+searchKeyUpObservable.pipe(
+    //Emit latest events every 0.5sec, when the user starts typing
+    auditTime(500),
 
-const demoControl = merge(start,stop).pipe(
-   startWith('stop'),
-   switchMap(command => (command == 'start')? sampleToSave : EMPTY)
-).subscribe();                     
+    //search for typeahead suggestions
+    tap(event => console.log(" Searching for : "+ event.target.value))
+).subscribe();                    
 
